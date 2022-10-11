b8599b582e24d842897f68d12f7e0de0f86d2ecb
Muhammadsulton Sodikov <Sodikov-288@yandex.ru>
2022-10-11 16:34:22 +0300

    first version

---
+++ templates/src/train.py
@@ -0,0 +1,86 @@
+import configparser
+import os
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import sys
+import traceback
+import warnings
+warnings.filterwarnings('ignore')
+
+from logger import Logger
+
+SHOW_LOG = True
+
+
+class MultiModel():
+
+    def __init__(self) -> None:
+        logger = Logger(SHOW_LOG)
+        self.config = configparser.ConfigParser()
+        self.log = logger.get_logger(__name__)
+        self.config.read("config.ini")
+        self.X_train = pd.read_csv(
+            self.config["SPLIT_DATA"]["X_train"], index_col=0)
+        self.y_train = pd.read_csv(
+            self.config["SPLIT_DATA"]["y_train"], index_col=0)
+        self.X_test = pd.read_csv(
+            self.config["SPLIT_DATA"]["X_test"], index_col=0)
+        self.y_test = pd.read_csv(
+            self.config["SPLIT_DATA"]["y_test"], index_col=0)
+        sc = StandardScaler()
+        self.X_train = sc.fit_transform(self.X_train)
+        self.X_test = sc.transform(self.X_test)
+        self.project_path = os.path.join(os.getcwd(), "../experiments")
+        self.log_reg_path = os.path.join(self.project_path, "log_reg.sav")
+        self.rand_forest_path = os.path.join(
+            self.project_path, "rand_forest.sav")
+        self.log.info("MultiModel is ready")
+
+    def log_reg(self, predict=False) -> bool:
+        classifier = LogisticRegression()
+        try:
+            classifier.fit(self.X_train,self.y_train)
+        except Exception:
+            self.log.error(traceback.format_exc())
+        if predict:
+            y_pred = classifier.predict(self.X_test)
+            print(accuracy_score(self.y_test, y_pred))
+        params = {'path':self.log_reg_path}
+        return self.save_model(classifier, self.log_reg_path, "LOG_REG", params)
+
+    def rand_forest(self, n_trees=100, criterion="entropy", predict=False) -> bool:
+        classifier = RandomForestClassifier(n_estimators=n_trees, criterion=criterion)
+        try:
+            classifier.fit(self.X_train,self.y_train)
+        except  Exception:
+            self.log.error(traceback.format_exc())
+            sys.exit(1)
+        if predict:
+            y_pred = classifier.predict(self.X_test)
+            print(accuracy_score(self.y_test, y_pred))
+        params = {'n_estimators':n_trees,
+                      'criterion':criterion,
+                      'path': self.rand_forest_path}
+        return self.save_model(classifier, self.rand_forest_path, "RAND_FOREST", params)
+
+    def save_model(self, classifier, path:str, name:str,params:dict) -> bool:
+        self.config[name] = params
+        os.remove('config.ini')
+        with open('config.ini','w') as configfile:
+            self.config.write(configfile)
+        pickle.dump(classifier, open(path,'wb'))
+        self.log.info(f'{path} is saved')
+        return os.path.isfile(path)
+
+if __name__ == '__main__':
+    multi_model = MultiModel()
+    multi_model.log_reg(predict=True)
+    multi_model.rand_forest(predict=True)
